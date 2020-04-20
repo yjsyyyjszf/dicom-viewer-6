0ade5e94383bf39fc941903aecfe9166804b6892
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-20 22:51:48 +0200

    Create Header component

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './header.css';
+
+
+function Header() {
+  return (
+    <header className="header">
+      <h1> dicom-viewer </h1>
+    </header>
+  )
+};
+
+export default Header;
